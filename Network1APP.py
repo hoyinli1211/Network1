96504cc94d45d45e23331c2536b8f67eb26f89fd
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-09 22:56:23 +0800

    Update Network1APP.py

--- Network1APP.py
+++ Network1APP.py
@@ -61,7 +61,7 @@ else:
   st.write(df_edge_select)
   
   #Create networkx graph object from pandas dataframe
-  G = nx.from_pandas_edgelist(df=df_edge_select, source='Orig', target='Dest', edge_attr=['weight', 'title'], create_using=nx.DiGraph())
+  G = nx.from_pandas_edgelist(df=df_edge_select, source='Orig', target='Dest', node_attr=['color'], edge_attr=['weight', 'title'], create_using=nx.DiGraph())
   net = Network(height='465px', bgcolor='#222222', font_color='white', directed=True)
   # Take Networkx graph and translate it to a PyVis graph format
   net.from_nx(G)
