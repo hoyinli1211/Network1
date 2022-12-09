86dacaa9feb7898838b7b85f9e663ae33ff25149
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-09 11:19:54 +0800

    Update Network1APP.py

--- Network1APP.py
+++ Network1APP.py
@@ -3,6 +3,7 @@
 
 #import required library
 import streamlit as st
+import streamlit.components.v1 as components
 import os, sys
 import pandas as pd
 import networkx as nx
@@ -21,7 +22,49 @@ file_temp = 'Network1.xlsx'
 df_node_temp = pd.read_excel(file_temp, 'node')
 df_edge_temp = pd.read_excel(file_temp, 'edge')
 
+df_node = df_node_temp
+df_edge = df_edge_temp
+
 st.title('Node Data')
 st.write(df_node_temp)
 st.title('Edge Data')
 st.write(df_edge_temp)
+
+#Define list of selection options
+acct_list = df_node_temp['name']
+acct_list.sort()
+#Implement multiselect dropdown menu for option selection (returns a list)
+selected_acct = st.multiselect('Select acct(s) to visualize', acct_list)
+
+#Set info message on initial site load
+if len(selected_acct)==0:
+  st.text('Choose at least 1 account to get started')
+else:
+  df_edge_select = df_edge.loc[df_edge['Orig'].isin(selected_acct) | df_edge['Dest'].isin(selected_acct)]
+  df_edge_select = df_node_select.reset_index(drop=True)
+  
+  #Create networkx graph object from pandas dataframe
+  G = nx.from_pandas_edgelist(df_edge_select, 'Org', 'Dest', 'Value')
+  net = Network(height='465px', bgcolor='#222222', font_color='white')
+  # Take Networkx graph and translate it to a PyVis graph format
+  net.from_nx(G)
+  # Generate network with specific layout settings
+  net.repulsion(node_distance=420,
+                central_gravity=0.33,
+                spring_length=110,
+                spring_strength=0.10,
+                damping=0.95)
+  # Save and read graph as HTML file (on Streamlit Sharing)
+  try:
+    path = '/tmp'
+    net.save_graph(f'{path}/pyvis_graph.html')
+    HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+
+  # Save and read graph as HTML file (locally)
+  except:
+    path = '/html_files'
+    net.save_graph(f'{path}/pyvis_graph.html')
+    HtmlFile = open(f'{path}/pyvis_graph.html', 'r', encoding='utf-8')
+
+  # Load HTML file in HTML component for display on Streamlit page
+  components.html(HtmlFile.read(), height=435)
