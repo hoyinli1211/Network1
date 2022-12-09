9a143a691dddceec1d53dd5640613d12efd0c9b3
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-09 22:33:04 +0800

    Update Network1APP.py

--- Network1APP.py
+++ Network1APP.py
@@ -28,7 +28,7 @@ df_node = df_node_temp
 df_edge = df_edge_temp
 
 df_edge['weight'] = len(str(df_edge['Amount']))
-df_edge['title'] = df_edge['Orig'] + ' (' + df_edge['Orig.Bank'] + ') transferred HK$' + "{:,}".format(df_edge['Amount']) + ' to ' + df_edge['Dest'] + ' (' + df_edge['Dest.Bank'] + ')'
+df_edge['title'] = df_edge['Orig'] + ' (' + df_edge['Orig.Bank'] + ') transferred HK$' + df_edge['Amount'] + ' to ' + df_edge['Dest'] + ' (' + df_edge['Dest.Bank'] + ')'
 
 st.title('Node Data')
 st.write(df_node)
