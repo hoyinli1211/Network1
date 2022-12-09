0ed9938dd28edb7413f5b161a982a890231bb468
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-09 11:07:04 +0800

    Create Network1APP.py

---
+++ Network1APP.py
@@ -0,0 +1,27 @@
+#20221209 - streamlit with network visualization
+#Ref: https://medium.com/towards-data-science/how-to-deploy-interactive-pyvis-network-graphs-on-streamlit-6c401d4c99db
+
+#import required library
+import streamlit as st
+import os, sys
+import pandas as pd
+import networkx as nx
+from pyvis.network import Network
+
+#initialization of session state
+if 'indEnd' not in st.session_state:
+  st.session_state['ind0'] = False
+  
+# Add a title and intro text
+st.title('Fund Flow Network Visualization using streamlit')
+st.text('This is the first attempt to use PyVis package to perform interactive network visualization in streamlit platform')
+
+#Import template file
+file_temp = 'template.xlsx'
+df_node_temp = pd.read_excel(file_temp, 'node')
+df_edge_temp = pd.read_excel(file_temp, 'edge')
+
+st.title('Node Data')
+st.write(df_node_temp)
+st.title('Edge Data')
+st.write(df_edge_temp)
