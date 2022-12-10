5e5de7ded3cfb32f88f3f42e26c5615dcfd501d1
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-10 15:36:29 +0800

    Update Network1APP.py

--- Network1APP.py
+++ Network1APP.py
@@ -90,8 +90,6 @@ elif (len(selected_onus_acct)>0 or len(selected_offus_acct)>0):
   st.write(G2.nodes)
   color_G2node = ['blue', 'red', 'red', 'orange', 'blue', 'blue', 'orange']
   G2 = nx.set_node_attributes(G2, color_G2node, 'node_color')
-  st.write(G2.nodes)
-  st.write(G2.nodes['node_color'])
   net2 = Network(height='465px', bgcolor='#222222', font_color='white', directed=True, node_color='red')
   net2.from_nx(G2)
   net2.save_graph(f'pyvis_graph.html')
