aa31f2bf69e4a1a74262ba52577a97dc89c9ee01
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-10 12:37:42 +0800

    Update Network1APP.py

--- Network1APP.py
+++ Network1APP.py
@@ -35,15 +35,17 @@ df_onus = pd.concat([df_edge.loc[df_edge['Orig.Bank']=='on-us']['Orig'],
                     df_edge.loc[df_edge['Dest.Bank']=='on-us']['Dest']],
                     ignore_index=True,
                     axis=0).drop_duplicates()
+df_onus.rename(columns = {'0':'name'}, inplace=True)
 
-df_offus = pd.concat([df_edge.loc[df_edge['Orig.Bank']!='on-us']['Orig'],
-                    df_edge.loc[df_edge['Dest.Bank']!='on-us']['Dest']],
+df_offus = pd.concat(([df_edge.loc[df_edge['Orig.Bank']!='on-us']['Orig'],
+                    df_edge.loc[df_edge['Dest.Bank']!='on-us']['Dest']], s.rename('name')),
                     ignore_index=True,
                     axis=0).drop_duplicates()
 
 st.title('Edge Data')
 st.write(df_edge)
 st.write(df_onus)
+st.write(df_offus)
 
 #Define show first layer only, or second layer as well
 nlayer = st.radio("Number of layer",
