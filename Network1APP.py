b6e876aba5904336044e0d129ae5f6f86871e969
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-10 16:48:14 +0800

    Update Network1APP.py

--- Network1APP.py
+++ Network1APP.py
@@ -143,47 +143,5 @@ elif (len(selected_onus_acct)>0 or len(selected_offus_acct)>0):
 else:
   pass
    
-"""
-#Set info message on initial site load
-if len(selected_acct)==0:
-  st.text('Choose at least 1 account to get started')
-else:
-  if nlayer == 'first layer only':
-    df_edge_select = df_edge.loc[df_edge['Orig'].isin(selected_acct) | df_edge['Dest'].isin(selected_acct)]
-  elif nlayer == 'with second layer':
-    df_edge_select0 = df_edge.loc[df_edge['Orig'].isin(selected_acct) | df_edge['Dest'].isin(selected_acct)]
-    selected_acct_second = pd.concat([df_edge_select0['Orig'], df_edge_select0['Dest']], axis=0).drop_duplicates()
-    df_edge_select = df_edge.loc[df_edge['Orig'].isin(selected_acct_second) | df_edge['Dest'].isin(selected_acct_second)]
-  else:
-    pass
-  #df_edge_select = df_edge_select.reset_index(drop=True)
-  st.write(df_edge_select)
+#End of Script  
   
-  #Create networkx graph object from pandas dataframe
-  G = nx.from_pandas_edgelist(df=df_edge_select, source='Orig', target='Dest', edge_attr=['weight', 'title'], create_using=nx.DiGraph())
-  st.write(G.nodes)
-  net = Network(height='465px', bgcolor='#222222', font_color='white', directed=True)
-  # Take Networkx graph and translate it to a PyVis graph format
-  net.from_nx(G)
-  
-  # Generate network with specific layout settings
-  net.repulsion(node_distance=420,
-                central_gravity=0.33,
-                spring_length=110,
-                spring_strength=0.10,
-                damping=0.95)
-  # Save and read graph as HTML file (on Streamlit Sharing)
-  try:
-    path = '/tmp'
-    net.save_graph(f'pyvis_graph.html')
-    HtmlFile = open(f'pyvis_graph.html', 'r', encoding='utf-8')
-
-  # Save and read graph as HTML file (locally)
-  except:
-    path = '/html_files'
-    net.save_graph(f'pyvis_graph.html')
-    HtmlFile = open(f'pyvis_graph.html', 'r', encoding='utf-8')
-
-  # Load HTML file in HTML component for display on Streamlit page
-  components.html(HtmlFile.read(), height=435)
-"""
