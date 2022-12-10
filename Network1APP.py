cefa1e0b3cce057f7f1d21fb7f73bbd50a9b5561
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-10 15:33:02 +0800

    Update Network1APP.py

--- Network1APP.py
+++ Network1APP.py
@@ -89,7 +89,8 @@ elif (len(selected_onus_acct)>0 or len(selected_offus_acct)>0):
   G2 = nx.from_pandas_edgelist(df_edge_firstlayer, source='Orig', target='Dest', edge_attr=['weight', 'title'], create_using=nx.DiGraph())
   st.write(G2.nodes)
   color_G2node = ['blue', 'red', 'red', 'orange', 'blue', 'blue', 'orange']
-  nx.set_node_attributes(G2, color_G2node, 'node_color')
+  G2 = nx.set_node_attributes(G2, color_G2node, 'node_color')
+  st.write(G2.nodes)
   st.write(G2.nodes['node_color'])
   net2 = Network(height='465px', bgcolor='#222222', font_color='white', directed=True)
   net2.from_nx(G2)
