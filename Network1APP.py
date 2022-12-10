9ee5ce8934f46394a9950ffb4a33757a6eebbeee
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-10 14:13:10 +0800

    Update Network1APP.py

--- Network1APP.py
+++ Network1APP.py
@@ -80,7 +80,7 @@ elif (len(selected_onus_acct)>0 or len(selected_offus_acct)>0):
   df_edge_firstlayer = df_edge.loc[df_edge['Orig'].isin(firstlayer_acct) | df_edge['Dest'].isin(firstlayer_acct)]
   firstlayer_onus_acct = pd.concat([df_edge_firstlayer.loc[df_edge_firstlayer['Orig.Bank']=='on-us']['Orig'].drop_duplicates(),
                          df_edge_firstlayer.loc[df_edge_firstlayer['Dest.Bank']=='on-us']['Dest'].drop_duplicates()], axis=0).drop_duplicates().rename('name')
-  firstlayer_onus_acct.tolist()
+  firstlayer_onus_acct = firstlayer_onus_acct.tolist()
   st.write(type(firstlayer_onus_acct))
   firstlayer_new_onus_acct = pd.concat([df_edge_firstlayer.loc[df_edge_firstlayer['Orig.Bank']=='on-us' & df_edge_firstlayer['Orig'] not in firstlayer_acct]['Orig'].drop_duplicates(),
                          df_edge_firstlayer.loc[df_edge_firstlayer['Dest.Bank']=='on-us' & df_edge_firstlayer['Dest'] not in firstlayer_acct]['Dest'].drop_duplicates()], axis=0).drop_duplicates().rename('name')
