2db6005d1a6063ae78da1d098580a1008520a477
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-12 21:23:01 +0800

    Update Network1APP.py

--- Network1APP.py
+++ Network1APP.py
@@ -112,7 +112,7 @@ elif (len(selected_onus_acct)>0 or len(selected_offus_acct)>0):
     st.write(df_edge_fraud)
     G1 = nx.from_pandas_edgelist(df=df_edge_fraud, source='Orig', target='Dest', edge_attr=['weight', 'title'], create_using=nx.DiGraph())
     nx.set_node_attributes(G1, df_node_fraud.set_index('name')['color'].to_dict(), 'color')
-    nx.set_node_attributes(G2, df_node_fraud.set_index('name')['label'].to_dict(), 'label')
+    nx.set_node_attributes(G1, df_node_fraud.set_index('name')['label'].to_dict(), 'label')
     net1 = Network(height='465px', bgcolor='#222222', font_color='white', directed=True)
     # Take Networkx graph and translate it to a PyVis graph format
     net1.from_nx(G1)
