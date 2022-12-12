0eabf070c262dfdb958666556b99e20bd7558214
hoyinli1211 <hoyinli.r@gmail.com>
2022-12-12 23:29:45 +0800

    Update Network1APP.py

--- Network1APP.py
+++ Network1APP.py
@@ -93,7 +93,7 @@ elif (len(selected_onus_acct)>0 or len(selected_offus_acct)>0):
   firstlayer_offus_acct = pd.concat([df_edge_firstlayer.loc[df_edge_firstlayer['Orig.Bank']!='on-us']['Orig'].drop_duplicates(),
                           df_edge_firstlayer.loc[df_edge_firstlayer['Dest.Bank']!='on-us']['Dest'].drop_duplicates()], axis=0).drop_duplicates().rename('name')
   firstlayer_offus_acct = firstlayer_offus_acct.tolist()
-  df_node_firstlayer = pd.DataFrame(firstlayer_onus_acct + firstlayer_new_onus_acct, columns=['name'])
+  df_node_firstlayer = pd.DataFrame(firstlayer_acct + firstlayer_new_onus_acct, columns=['name'])
   df_node_firstlayer['title'] = df_node_firstlayer['name'].apply(lambda x: 'onus Orig' if x in selected_onus_acct else ('offus Orig' if x in selected_offus_acct else ('onus 1st' if x in firstlayer_new_onus_acct else 'offus 1st')))
   df_node_firstlayer['color'] = df_node_firstlayer['name'].apply(lambda x: 'red' if x in selected_onus_acct else ('purple' if x in selected_offus_acct else ('orange' if x in firstlayer_new_onus_acct else 'blue')))
   st.write(df_node_firstlayer)
